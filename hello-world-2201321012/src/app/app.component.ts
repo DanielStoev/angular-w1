ae9b55ea0a0aa2ae40250b198bdbf8db2ad703f4
DanielStoev <dannybob63@gmail.com>
2024-04-23 23:18:38 +0300

    added logic

--- hello-world-2201321012/src/app/app.component.ts
+++ hello-world-2201321012/src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgFor, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'hello-world-2201321012';
+  showAdditionalInfo: number = 0;
+
+  toggleAdditionalInfo(index: number): void {
+    if (this.showAdditionalInfo === index) { 
+      this.showAdditionalInfo = 0;
+    } else {
+      this.showAdditionalInfo = index;
+    }
+  }
 }
